495e74c293da0f992dd0d9ebbf193a525b870016
smenend1 <smenend1@xtec.cat>
2026-06-11 11:07:28 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# PortesLogiques
+# Portes Lògiques Plus v2.3
+
+PWA de laboratori visual de portes lògiques.
+
+## Novetat v2.3
+
+- A l'apartat Circuits s'afegeix una tercera porta NOT al final del logigrama.
+- El circuit queda com: G1(A,B) -> X; G2(X,C) -> Y; NOT(Y) -> S.
+- La taula de veritat incorpora les columnes X, Y i S = NOT(Y).
+- Es manté la impressió docent corregida i el logigrama visual.
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='portes-logiques-plus-v2-1.1';
-const ASSETS=['./','./index.html','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const CACHE='portes-logiques-plus-v2-3';
+const ASSETS=['./','./index.html','./manifest.json','./sw.js','./assets/icon-192.png','./assets/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
