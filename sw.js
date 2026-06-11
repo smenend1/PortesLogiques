78e66ae9424bfd51e14dcc3e49dcd516ec9cc7f9
smenend1 <smenend1@xtec.cat>
2026-06-11 10:48:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='portes-logiques-plus-v2';
+const CACHE='portes-logiques-plus-v2-1.1';
 const ASSETS=['./','./index.html','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
